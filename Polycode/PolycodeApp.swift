2945056e86d8a8ec12ed5baa7d078ad4796064dc
Eric Yu <yueric2017@gmail.com>
2025-04-10 07:30:02 -0400

    Initial Commit

---
+++ Polycode/PolycodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolycodeApp.swift
+//  Polycode
+//
+//  Created by Eric Yu on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PolycodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
